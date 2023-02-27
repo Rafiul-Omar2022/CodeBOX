57e5def0107cfb77cfcacc643469a424b380936e
RafiulOmar2022 <111574212+Rafiul-Omar2022@users.noreply.github.com>
2023-02-27 20:35:09 +0600

    new projects assign - TaliBook

---
+++ c_c++/TalliBook.cpp
@@ -0,0 +1,18 @@
+/*
+* Author: Rafiul Omar Rafi
+* Project: TaliBook
+* Description: A C++ program to store monthly expenses
+* Starting Date: 27-02-2023
+* Open source, you can use.
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    printf("under constraction");
+
+    return 0;
+}
